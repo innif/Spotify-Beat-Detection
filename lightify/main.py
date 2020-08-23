1d0c5a1be3120add6d4965107cddb240d3de3f3e
Finn Harms <post@finn-harms.de>
2020-08-23 17:53:59 +0200

    minor changes

--- lightify/main.py
+++ lightify/main.py
@@ -15,7 +15,7 @@ apiKey = json.load(open("apiKey.json"))
 auth_manger = SpotifyOAuth(scope=apiKey['scope'], username=settings['username'], client_id=apiKey['id'], client_secret=apiKey['secret'], redirect_uri=apiKey['redirect_uri'])
 client = spotipy.Spotify(auth_manager=auth_manger)
 
-current_playback = client.current_playback()
+current_playback = client.currently_playing()
 current_song = current_playback['item']
 current_analysis = client.audio_analysis(current_song['id'])
 
@@ -26,8 +26,8 @@ class timeCheckerThread (threading.Thread):
     def run(self):
         global song_start_time, song_start_time_lock
         while True:
-            playback_pos = client.current_playback()['progress_ms']
             curr_ms = time.time_ns()/1000000
+            playback_pos = client.currently_playing()['progress_ms']
             start_time_temp = curr_ms - playback_pos
             song_start_time_lock.acquire()
             if song_start_time < start_time_temp or song_start_time == 0:
@@ -45,9 +45,6 @@ for event_string in event_types:
     event_lists[event_string] = current_analysis[event_string]
     event_lists[event_string].reverse()
     next_event[event_string] = event_lists[event_string].pop()['start']*1000
-
-while song_start_time == 0:
-    pass
 
 while True:
     song_start_time_lock.acquire()
